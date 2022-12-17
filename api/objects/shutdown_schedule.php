c362417e249a590ee911b196855d41e1addb9418
DartVeyder <medvsd.dmitro@gmail.com>
2022-12-17 20:04:42 +0200

    Відображня графіку відключення по групах

--- api/objects/shutdown_schedule.php
+++ api/objects/shutdown_schedule.php
@@ -5,10 +5,15 @@
         private $conn;
         private $table_name = "shutdown_schedule";
 
-        private $group_id;
-        private $weekday_id;
-        private $time_id;
-        private $status_id;
+        public $group_id;
+        public $weekday_id;
+        public $time_id;
+        public $status_id;
+        public $group_name;
+        public $weekday_name;
+        public $shutdown_time;
+        public $power_time;
+        public $status_name;
 
         // конструктор для підключення до бази даних 
         public function __construct($db)
@@ -52,8 +57,43 @@
         }
 
         //метод для получення графіку відключення по групах
-        private function readGroup(){
+        public function readGroup(){
+            $query = "SELECT 
+                
+            s.weekday_id, s.group_id, s.time_id, s.status_id, 
+            st.name as status_name, 
+            t.shutdown_time, t.power_time,
+            gr.name as group_name,
+            w.name as weekday_name
+            FROM 
+                ".$this->table_name." s
+                LEFT JOIN
+                    weekdays w
+                        ON s.weekday_id = w.weekday_id
+                LEFT JOIN
+                    time t 
+                        ON s.time_id = t.time_id
+                LEFT JOIN
+                    status st
+                        ON s.status_id = st.status_id
+                LEFT JOIN
+                    cluster gr
+                        on s.group_id = gr.group_id 
+                WHERE
+                    s.group_id = ?
+                ORDER BY
+                s.group_id ASC , s.weekday_id ASC";
+            ;
+                    
+            // подготовка запроса
+            $stmt = $this->conn->prepare($query);
 
+            // привязываем id товара, который будет получен
+            $stmt->bindParam(1, $this->group_id);
+
+            // выполняем запрос
+            $stmt->execute(); 
+            return $stmt;
         }
 
         //метод для получення часу наступного відключення
---
+++ api/shutdown_schedule/read_group.php
@@ -0,0 +1,65 @@
+<?php
+    // необходимые HTTP-заголовки
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: access");
+    header("Access-Control-Allow-Methods: GET");
+    header("Access-Control-Allow-Credentials: true");
+    header("Content-Type: application/json");
+
+    // подключение базы данных и файл, содержащий объекты
+    include_once "../../class/dataBase/database.php";
+    include_once "../objects/shutdown_schedule.php";
+
+    // получаем соединение с базой данных
+    $database = new Database();
+    $db = $database->getConnection();
+
+    // инициализируем объект
+    $shutdown_schedule = new ShutdownShedule($db);
+    
+    // установим свойство ID записи для чтения
+    $shutdown_schedule->group_id = isset($_GET["group_id"]) ? $_GET["group_id"] : die();
+
+    // получим графік відключення по групі
+    $stmt = $shutdown_schedule->readGroup();
+
+    $num = $stmt->rowCount();
+
+
+    if($num > 0){
+        $shutdown_schedule_arr = array();
+        $shutdown_schedule_arr['records'] = array();
+
+        // получаем содержимое нашей таблицы
+        // fetch() быстрее, чем fetchAll()
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            // извлекаем строку
+            extract($row);
+            $shutdown_schedule_item = array(
+                "group_id" => $group_id,
+                "group_name" => $group_name,
+                "weekday_id" => $weekday_id,
+                "weekday_name" => $weekday_name,
+                "shutdown_time" => $shutdown_time,
+                "power_time" => $power_time,
+                "status_id" => $status_id,
+                "status_name" => $status_name
+            );
+            array_push($shutdown_schedule_arr["records"], $shutdown_schedule_item);
+        }
+
+        // устанавливаем код ответа - 200 OK
+        http_response_code(200);
+
+        // выводим данные о товаре в формате JSON
+    // echo json_encode($shutdown_schedule_arr);
+    print_r($shutdown_schedule_arr);
+    }else {
+        // установим код ответа - 404 Не найдено
+        http_response_code(404);
+
+        // сообщаем пользователю, что товары не найдены
+        echo json_encode(array("message" => "Графіка виключень не знайдено."), JSON_UNESCAPED_UNICODE);
+    }
+
+?>
