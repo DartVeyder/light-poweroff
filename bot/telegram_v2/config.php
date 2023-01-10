b9199ae6e8b774f9cb64cacccf75c0f30d1d8cff
Dmytro Medvid <95388110+DartVeyder@users.noreply.github.com>
2023-01-10 22:37:15 +0200

    Delete config.php

--- bot/telegram_v2/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-const DEV        = true;
-const TOKEN_DEV = "5953514111:AAHTQGQnJ8ek0cTq8NZd7PkFqpDmikx8xwQ";
-const TOKEN_PROD = "5794448735:AAFXHPmsnsYIgTfxmBXs09SpX9_QyTzyd1w";
-const TIMEZONE   = "Europe/Kiev"; 
