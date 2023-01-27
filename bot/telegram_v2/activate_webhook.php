7d0e054c3fbfc02ea406b113d4416d93d6ebb642
DartVeyder <medvsd.dmitro@gmail.com>
2023-01-27 16:37:42 +0200

    ...

--- bot/telegram_v2/activate_webhook.php
+++ bot/telegram_v2/activate_webhook.php
@@ -1,5 +1,5 @@
 <?php
 include_once "config.php";
-$url = URL_BOT . 'telegram_v2/';
+$url = URL_BOT . TELEGRAM_BOT_DIR;
 $url_webhook = "https://api.telegram.org/bot".TOKEN_DEV."/setWebHook?url=".$url;
 header("Location: $url_webhook");
