c36c64da363147edec0ea674f3308987a04340a1
DartVeyder <medvsd.dmitro@gmail.com>
2023-02-07 17:43:49 +0200

    дороблено сповіщення

--- bots/telegram/v2/dev/models/model_notification_next_shutdown.php
+++ bots/telegram/v2/dev/models/model_notification_next_shutdown.php
@@ -5,20 +5,32 @@ class Model_notification_next_shutdown extends Model
     { 
         $next_shutdown = Core::get("/shutdown_schedule/read_next_v2.php");
         $lang_text   = Service_text::get_message_text();
+        $info = [];
         if(date("20:30") ==  $next_shutdown['notification'])
             foreach ($next_shutdown['list'] as $item) {
-
                 $text =  "<b>➤" . $item['time_start'] . " - " . $item['time_end'] . " " .  $item['status_name'] . "</b>"; 
                 $button = [['text' =>  $lang_text['button_notification_off'], 'callback_data' => 'update-nns_0']]; 
                 $button_merge =  [[['text' =>   $lang_text['button_to_shedule'], 'callback_data' => 'shedule']]];
                 $data = self::message($text, $button, $button_merge);
-
-                try {
-                    Helper::send($text);
-                } catch (Exception $e) {
-
+                $data['action'] = $action;
+                try { 
+                    View_notification_next_shutdown::index($data, $item['user_telegram_id']); 
+                    $active = 1;
+                    $status = 'Відправлено'; 
+                } catch (Exception $e) {  
+                    $active = 0;
+                    $status = 'Не відправлено';
+                    $error  = $e->getMessage();
                 }
+
+                $info[] = [
+                    "user_telegram_id" => $item['user_telegram_id'],
+                    "active" => $active,
+                    "status" => $status, 
+                    "error" => $error 
+                ];  
             }
+            Helper::dd($info, false); 
     }
 
     public static function update($notification_id){
