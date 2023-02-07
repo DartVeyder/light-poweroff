1e2e0f195f8d91984f05cb58040d41c8334c7911
DartVeyder <medvsd.dmitro@gmail.com>
2023-02-07 17:16:50 +0200

    ...

--- bots/telegram/v2/dev/models/model_notification_next_shutdown.php
+++ bots/telegram/v2/dev/models/model_notification_next_shutdown.php
@@ -3,8 +3,22 @@ class Model_notification_next_shutdown extends Model
 {
     public static function index($action)
     { 
-        $next_shutdown = Core::get("/shutdown_schedule/read_next.php");
-        Helper::send($next_shutdown);
+        $next_shutdown = Core::get("/shutdown_schedule/read_next_v2.php");
+        $lang_text   = Service_text::get_message_text();
+        if(date("20:30") ==  $next_shutdown['notification'])
+            foreach ($next_shutdown['list'] as $item) {
+
+                $text =  "<b>➤" . $item['time_start'] . " - " . $item['time_end'] . " " .  $item['status_name'] . "</b>"; 
+                $button = [['text' =>  $lang_text['button_notification_off'], 'callback_data' => 'update-nns_0']]; 
+                $button_merge =  [[['text' =>   $lang_text['button_to_shedule'], 'callback_data' => 'shedule']]];
+                $data = self::message($text, $button, $button_merge);
+
+                try {
+                    Helper::send($text);
+                } catch (Exception $e) {
+
+                }
+            }
     }
 
     public static function update($notification_id){
